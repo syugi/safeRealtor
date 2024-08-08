354badb25ed9c35726c8dfa9fc9710b1fe3a5729
misookHeo <msheo@astronsec.com>
2024-08-08 17:32:28 +0900

    init

---
+++ src/test/java/com/loadone/safeRealtor/SafeRealtorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadone.safeRealtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeRealtorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
