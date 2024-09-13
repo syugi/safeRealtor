82008a4da16db5ed8cbad12186474963ad3f2d94
syugi <altnr23@gmail.com>
2024-09-13 16:02:45 +0900

    주석 추가

--- src/main/java/com/loadone/saferealtor/controller/FavoriteController.java
+++ src/main/java/com/loadone/saferealtor/controller/FavoriteController.java
@@ -16,18 +16,21 @@ import java.util.List;
 public class FavoriteController {
     private final FavoriteService favoriteService;
 
+    // 찜하기
     @PostMapping("/add")
     public ResponseEntity<Void> addFavorite(@RequestBody FavoriteReqDTO request) {
         favoriteService.addFavorite(request.getUserId(), request.getPropertyId());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    // 찜 취소
     @DeleteMapping("/remove")
     public ResponseEntity<Void> removeFavorite(@RequestBody FavoriteReqDTO request) {
         favoriteService.removeFavorite(request.getUserId(), request.getPropertyId());
         return ResponseEntity.ok().build();
     }
 
+    // 찜한 매물 조회
     @GetMapping("/{userId}")
     public ResponseEntity<List<Property>> getFavoriteProperties(@PathVariable String userId) {
         List<Property> properties = favoriteService.getFavoriteProperties(userId);
